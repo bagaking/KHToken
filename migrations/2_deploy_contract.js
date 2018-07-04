8d616eb2a772d85cb98cc4f8a4c8e0ae37f610d6
bagaking <zfaustk@qq.com>
2018-07-04 17:26:00 +0800

    Ref Standard Implementation

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,4 +1,9 @@
 let KHToken = artifacts.require("KHToken");
+let KHToken_EIP20 = artifacts.require("KHToken_EIP20");
+let KHToken_StandardToken = artifacts.require("KHToken_StandardToken");
+let TAT = artifacts.require("TAT");
 module.exports = function(deployer) {
-  deployer.deploy(KHToken, "The Kh Token No.X", "KHTX", 10000000000, 18);
+  deployer.deploy(KHToken, "The Kh Token No.X", "KHT", 10000000000, 18);
+  deployer.deploy(KHToken_EIP20, "The Kh Token Standard", "KHTS", 10000000000, 18);
+  deployer.deploy(KHToken_StandardToken, "The Kh Token Standard", "KHTS", 10000000000, 18); 
 };
