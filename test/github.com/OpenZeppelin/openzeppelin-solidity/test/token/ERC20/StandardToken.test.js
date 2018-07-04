7f21abb2fe56e96df846632e70768f4e7323e67b
bagaking <zfaustk@qq.com>
2018-07-04 14:29:00 +0800

    Remove test of method we don't have

--- test/github.com/OpenZeppelin/openzeppelin-solidity/test/token/ERC20/StandardToken.test.js
+++ test/github.com/OpenZeppelin/openzeppelin-solidity/test/token/ERC20/StandardToken.test.js
@@ -272,202 +272,204 @@ contract(
     });
   });
 
-  describe('decrease approval', function () {
-    describe('when the spender is not the zero address', function () {
-      const spender = recipient;
+  // we dont have
 
-      describe('when the sender has enough balance', function () {
-        const amount = 100;
+  // describe('decrease approval', function () {
+  //   describe('when the spender is not the zero address', function () {
+  //     const spender = recipient;
 
-        it('emits an approval event', async function () {
-          const { logs } = await this.token.decreaseApproval(spender, amount, { from: owner });
+  //     describe('when the sender has enough balance', function () {
+  //       const amount = 100;
 
-          assert.equal(logs.length, 1);
-          assert.equal(logs[0].event, 'Approval');
-          assert.equal(logs[0].args.owner, owner);
-          assert.equal(logs[0].args.spender, spender);
-          assert(logs[0].args.value.eq(0));
-        });
+  //       it('emits an approval event', async function () {
+  //         const { logs } = await this.token.decreaseApproval(spender, amount, { from: owner });
 
-        describe('when there was no approved amount before', function () {
-          it('keeps the allowance to zero', async function () {
-            await this.token.decreaseApproval(spender, amount, { from: owner });
+  //         assert.equal(logs.length, 1);
+  //         assert.equal(logs[0].event, 'Approval');
+  //         assert.equal(logs[0].args.owner, owner);
+  //         assert.equal(logs[0].args.spender, spender);
+  //         assert(logs[0].args.value.eq(0));
+  //       });
 
-            const allowance = await this.token.allowance(owner, spender);
-            assert.equal(allowance, 0);
-          });
-        });
+  //       describe('when there was no approved amount before', function () {
+  //         it('keeps the allowance to zero', async function () {
+  //           await this.token.decreaseApproval(spender, amount, { from: owner });
 
-        describe('when the spender had an approved amount', function () {
-          beforeEach(async function () {
-            await this.token.approve(spender, amount + 1, { from: owner });
-          });
+  //           const allowance = await this.token.allowance(owner, spender);
+  //           assert.equal(allowance, 0);
+  //         });
+  //       });
 
-          it('decreases the spender allowance subtracting the requested amount', async function () {
-            await this.token.decreaseApproval(spender, amount, { from: owner });
+  //       describe('when the spender had an approved amount', function () {
+  //         beforeEach(async function () {
+  //           await this.token.approve(spender, amount + 1, { from: owner });
+  //         });
 
-            const allowance = await this.token.allowance(owner, spender);
-            assert.equal(allowance, 1);
-          });
-        });
-      });
+  //         it('decreases the spender allowance subtracting the requested amount', async function () {
+  //           await this.token.decreaseApproval(spender, amount, { from: owner });
 
-      describe('when the sender does not have enough balance', function () {
-        const amount = 101;
+  //           const allowance = await this.token.allowance(owner, spender);
+  //           assert.equal(allowance, 1);
+  //         });
+  //       });
+  //     });
 
-        it('emits an approval event', async function () {
-          const { logs } = await this.token.decreaseApproval(spender, amount, { from: owner });
+  //     describe('when the sender does not have enough balance', function () {
+  //       const amount = 101;
 
-          assert.equal(logs.length, 1);
-          assert.equal(logs[0].event, 'Approval');
-          assert.equal(logs[0].args.owner, owner);
-          assert.equal(logs[0].args.spender, spender);
-          assert(logs[0].args.value.eq(0));
-        });
+  //       it('emits an approval event', async function () {
+  //         const { logs } = await this.token.decreaseApproval(spender, amount, { from: owner });
 
-        describe('when there was no approved amount before', function () {
-          it('keeps the allowance to zero', async function () {
-            await this.token.decreaseApproval(spender, amount, { from: owner });
+  //         assert.equal(logs.length, 1);
+  //         assert.equal(logs[0].event, 'Approval');
+  //         assert.equal(logs[0].args.owner, owner);
+  //         assert.equal(logs[0].args.spender, spender);
+  //         assert(logs[0].args.value.eq(0));
+  //       });
 
-            const allowance = await this.token.allowance(owner, spender);
-            assert.equal(allowance, 0);
-          });
-        });
+  //       describe('when there was no approved amount before', function () {
+  //         it('keeps the allowance to zero', async function () {
+  //           await this.token.decreaseApproval(spender, amount, { from: owner });
 
-        describe('when the spender had an approved amount', function () {
-          beforeEach(async function () {
-            await this.token.approve(spender, amount + 1, { from: owner });
-          });
+  //           const allowance = await this.token.allowance(owner, spender);
+  //           assert.equal(allowance, 0);
+  //         });
+  //       });
 
-          it('decreases the spender allowance subtracting the requested amount', async function () {
-            await this.token.decreaseApproval(spender, amount, { from: owner });
+  //       describe('when the spender had an approved amount', function () {
+  //         beforeEach(async function () {
+  //           await this.token.approve(spender, amount + 1, { from: owner });
+  //         });
 
-            const allowance = await this.token.allowance(owner, spender);
-            assert.equal(allowance, 1);
-          });
-        });
-      });
-    });
+  //         it('decreases the spender allowance subtracting the requested amount', async function () {
+  //           await this.token.decreaseApproval(spender, amount, { from: owner });
 
-    describe('when the spender is the zero address', function () {
-      const amount = 100;
-      const spender = ZERO_ADDRESS;
+  //           const allowance = await this.token.allowance(owner, spender);
+  //           assert.equal(allowance, 1);
+  //         });
+  //       });
+  //     });
+  //   });
 
-      it('decreases the requested amount', async function () {
-        await this.token.decreaseApproval(spender, amount, { from: owner });
+  //   describe('when the spender is the zero address', function () {
+  //     const amount = 100;
+  //     const spender = ZERO_ADDRESS;
 
-        const allowance = await this.token.allowance(owner, spender);
-        assert.equal(allowance, 0);
-      });
+  //     it('decreases the requested amount', async function () {
+  //       await this.token.decreaseApproval(spender, amount, { from: owner });
 
-      it('emits an approval event', async function () {
-        const { logs } = await this.token.decreaseApproval(spender, amount, { from: owner });
+  //       const allowance = await this.token.allowance(owner, spender);
+  //       assert.equal(allowance, 0);
+  //     });
 
-        assert.equal(logs.length, 1);
-        assert.equal(logs[0].event, 'Approval');
-        assert.equal(logs[0].args.owner, owner);
-        assert.equal(logs[0].args.spender, spender);
-        assert(logs[0].args.value.eq(0));
-      });
-    });
-  });
+  //     it('emits an approval event', async function () {
+  //       const { logs } = await this.token.decreaseApproval(spender, amount, { from: owner });
 
-  describe('increase approval', function () {
-    const amount = 100;
+  //       assert.equal(logs.length, 1);
+  //       assert.equal(logs[0].event, 'Approval');
+  //       assert.equal(logs[0].args.owner, owner);
+  //       assert.equal(logs[0].args.spender, spender);
+  //       assert(logs[0].args.value.eq(0));
+  //     });
+  //   });
+  // });
 
-    describe('when the spender is not the zero address', function () {
-      const spender = recipient;
+  // describe('increase approval', function () {
+  //   const amount = 100;
 
-      describe('when the sender has enough balance', function () {
-        it('emits an approval event', async function () {
-          const { logs } = await this.token.increaseApproval(spender, amount, { from: owner });
+  //   describe('when the spender is not the zero address', function () {
+  //     const spender = recipient;
 
-          assert.equal(logs.length, 1);
-          assert.equal(logs[0].event, 'Approval');
-          assert.equal(logs[0].args.owner, owner);
-          assert.equal(logs[0].args.spender, spender);
-          assert(logs[0].args.value.eq(amount));
-        });
+  //     describe('when the sender has enough balance', function () {
+  //       it('emits an approval event', async function () {
+  //         const { logs } = await this.token.increaseApproval(spender, amount, { from: owner });
 
-        describe('when there was no approved amount before', function () {
-          it('approves the requested amount', async function () {
-            await this.token.increaseApproval(spender, amount, { from: owner });
+  //         assert.equal(logs.length, 1);
+  //         assert.equal(logs[0].event, 'Approval');
+  //         assert.equal(logs[0].args.owner, owner);
+  //         assert.equal(logs[0].args.spender, spender);
+  //         assert(logs[0].args.value.eq(amount));
+  //       });
 
-            const allowance = await this.token.allowance(owner, spender);
-            assert.equal(allowance, amount);
-          });
-        });
+  //       describe('when there was no approved amount before', function () {
+  //         it('approves the requested amount', async function () {
+  //           await this.token.increaseApproval(spender, amount, { from: owner });
 
-        describe('when the spender had an approved amount', function () {
-          beforeEach(async function () {
-            await this.token.approve(spender, 1, { from: owner });
-          });
+  //           const allowance = await this.token.allowance(owner, spender);
+  //           assert.equal(allowance, amount);
+  //         });
+  //       });
 
-          it('increases the spender allowance adding the requested amount', async function () {
-            await this.token.increaseApproval(spender, amount, { from: owner });
+  //       describe('when the spender had an approved amount', function () {
+  //         beforeEach(async function () {
+  //           await this.token.approve(spender, 1, { from: owner });
+  //         });
 
-            const allowance = await this.token.allowance(owner, spender);
-            assert.equal(allowance, amount + 1);
-          });
-        });
-      });
+  //         it('increases the spender allowance adding the requested amount', async function () {
+  //           await this.token.increaseApproval(spender, amount, { from: owner });
 
-      describe('when the sender does not have enough balance', function () {
-        const amount = 101;
+  //           const allowance = await this.token.allowance(owner, spender);
+  //           assert.equal(allowance, amount + 1);
+  //         });
+  //       });
+  //     });
 
-        it('emits an approval event', async function () {
-          const { logs } = await this.token.increaseApproval(spender, amount, { from: owner });
+  //     describe('when the sender does not have enough balance', function () {
+  //       const amount = 101;
 
-          assert.equal(logs.length, 1);
-          assert.equal(logs[0].event, 'Approval');
-          assert.equal(logs[0].args.owner, owner);
-          assert.equal(logs[0].args.spender, spender);
-          assert(logs[0].args.value.eq(amount));
-        });
+  //       it('emits an approval event', async function () {
+  //         const { logs } = await this.token.increaseApproval(spender, amount, { from: owner });
 
-        describe('when there was no approved amount before', function () {
-          it('approves the requested amount', async function () {
-            await this.token.increaseApproval(spender, amount, { from: owner });
+  //         assert.equal(logs.length, 1);
+  //         assert.equal(logs[0].event, 'Approval');
+  //         assert.equal(logs[0].args.owner, owner);
+  //         assert.equal(logs[0].args.spender, spender);
+  //         assert(logs[0].args.value.eq(amount));
+  //       });
 
-            const allowance = await this.token.allowance(owner, spender);
-            assert.equal(allowance, amount);
-          });
-        });
+  //       describe('when there was no approved amount before', function () {
+  //         it('approves the requested amount', async function () {
+  //           await this.token.increaseApproval(spender, amount, { from: owner });
 
-        describe('when the spender had an approved amount', function () {
-          beforeEach(async function () {
-            await this.token.approve(spender, 1, { from: owner });
-          });
+  //           const allowance = await this.token.allowance(owner, spender);
+  //           assert.equal(allowance, amount);
+  //         });
+  //       });
 
-          it('increases the spender allowance adding the requested amount', async function () {
-            await this.token.increaseApproval(spender, amount, { from: owner });
+  //       describe('when the spender had an approved amount', function () {
+  //         beforeEach(async function () {
+  //           await this.token.approve(spender, 1, { from: owner });
+  //         });
 
-            const allowance = await this.token.allowance(owner, spender);
-            assert.equal(allowance, amount + 1);
-          });
-        });
-      });
-    });
+  //         it('increases the spender allowance adding the requested amount', async function () {
+  //           await this.token.increaseApproval(spender, amount, { from: owner });
 
-    describe('when the spender is the zero address', function () {
-      const spender = ZERO_ADDRESS;
+  //           const allowance = await this.token.allowance(owner, spender);
+  //           assert.equal(allowance, amount + 1);
+  //         });
+  //       });
+  //     });
+  //   });
 
-      it('approves the requested amount', async function () {
-        await this.token.increaseApproval(spender, amount, { from: owner });
+  //   describe('when the spender is the zero address', function () {
+  //     const spender = ZERO_ADDRESS;
 
-        const allowance = await this.token.allowance(owner, spender);
-        assert.equal(allowance, amount);
-      });
+  //     it('approves the requested amount', async function () {
+  //       await this.token.increaseApproval(spender, amount, { from: owner });
 
-      it('emits an approval event', async function () {
-        const { logs } = await this.token.increaseApproval(spender, amount, { from: owner });
+  //       const allowance = await this.token.allowance(owner, spender);
+  //       assert.equal(allowance, amount);
+  //     });
 
-        assert.equal(logs.length, 1);
-        assert.equal(logs[0].event, 'Approval');
-        assert.equal(logs[0].args.owner, owner);
-        assert.equal(logs[0].args.spender, spender);
-        assert(logs[0].args.value.eq(amount));
-      });
-    });
-  });
+  //     it('emits an approval event', async function () {
+  //       const { logs } = await this.token.increaseApproval(spender, amount, { from: owner });
+
+  //       assert.equal(logs.length, 1);
+  //       assert.equal(logs[0].event, 'Approval');
+  //       assert.equal(logs[0].args.owner, owner);
+  //       assert.equal(logs[0].args.spender, spender);
+  //       assert(logs[0].args.value.eq(amount));
+  //     });
+  //   });
+  // });
 });
