7808cadd9aa2555b3d5cdb8ff03f452068709d4d
bagaking <zfaustk@qq.com>
2018-07-05 00:54:32 +0800

    Fix js code of deploy

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,7 +1,6 @@
 let KHToken = artifacts.require("KHToken");
 let KHToken_EIP20 = artifacts.require("KHToken_EIP20");
-let KHToken_StandardToken = artifacts.require("KHToken_StandardToken");
-let TAT = artifacts.require("TAT");
+let KHToken_StandardToken = artifacts.require("KHToken_StandardToken"); 
 module.exports = function(deployer) {
   deployer.deploy(KHToken, "The Kh Token No.X", "KHT", 10000000000, 18);
   deployer.deploy(KHToken_EIP20, "The Kh Token Standard", "KHTS", 10000000000, 18);
